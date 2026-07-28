68fbdf594a823013de50e230044b1ebd73cc9728
LAlcaide <108708146+LAlcaide@users.noreply.github.com>
2026-07-28 20:13:50 +0800

    Update dog_cooldown_controller.v

--- FPGA-Dog-Feeder/rtl/dog_cooldown_controller.v
+++ FPGA-Dog-Feeder/rtl/dog_cooldown_controller.v
@@ -3,7 +3,6 @@ module dog_cooldown_controller(clk, dogstart, cooldown_done);
 	input [2:0] dogstart;
 	output [2:0] cooldown_done;
 	
-	wire [2:0] cooldown_done_internal;
 	genvar i;
 	
 	generate
@@ -11,11 +10,9 @@ module dog_cooldown_controller(clk, dogstart, cooldown_done);
 			cooldown_timer TIMER_INST(
 				.clk(clk),
 				.dogstart(dogstart[i]),
-				.cooldown_done(cooldown_done_internal[i])
+				.cooldown_done(cooldown_done[i])
 			);
 		end
 	endgenerate
-	
-	assign cooldown_done = cooldown_done_internal;
 
 endmodule
