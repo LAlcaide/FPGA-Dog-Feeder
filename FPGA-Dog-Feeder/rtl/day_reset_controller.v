e4593c8362d0f4f64f7abfa1bc93a7ce93316a29
LAlcaide <108708146+LAlcaide@users.noreply.github.com>
2026-08-06 15:18:43 +0800

    Update day_reset_controller.v

--- FPGA-Dog-Feeder/rtl/day_reset_controller.v
+++ FPGA-Dog-Feeder/rtl/day_reset_controller.v
@@ -4,26 +4,29 @@ module day_reset_controller(clk, reset, resetFoodLog);
 	output reg resetFoodLog;
 	
 	reg [31:0] resetdel;
+	reg fired;
 	
 	localparam ONE_SECOND = 32'd50000000;
 
 	initial begin
-		{resetdel, resetFoodLog}<=0;
+		{resetdel, resetFoodLog, fired}<=0;
 	end
 
 	always @(posedge clk) begin
 		//DAY RESET CONTROLLER
 		//DAY RESET TRIGGERED BY ESP
-		if(@posedge reset) begin
-			resetdel<=resetdel+1;
+		resetFoodLog<=0;
+		if(!reset) begin
+			resetdel<= 0;
+            fired<= 0;
+		end
+		else if(!fired) begin
 			if(resetdel>(ONE_SECOND-1)*3) begin
 				resetFoodLog<=1;  
-				resetdel<=0;  
+				fired<=1;  
 			end
+			else
+				resetdel<=resetdel+1;
 		end
-		else begin
-			resetFoodLog<=0;
-			resetdel<=0;
-		end		
 	end
 endmodule
