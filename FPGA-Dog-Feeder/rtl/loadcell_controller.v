03865ffb7257bee575457b20dd085b6c5d322be6
LAlcaide <108708146+LAlcaide@users.noreply.github.com>
2026-08-06 15:32:38 +0800

    Update loadcell_controller.v

--- FPGA-Dog-Feeder/rtl/loadcell_controller.v
+++ FPGA-Dog-Feeder/rtl/loadcell_controller.v
@@ -1,3 +1,4 @@
+`default_nettype none
 module loadcell_controller(clk, loadcell, cloadcell, wloadcell, wcloadcell, loadcellreg, cloadcellreg, wloadcellreg, wcloadcellreg);
 	input clk;
 	input [1:0] loadcell, cloadcell, wloadcell, wcloadcell;
