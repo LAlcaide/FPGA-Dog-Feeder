5c1207e272f96bf068e681284c2b0e79627046eb
LAlcaide <108708146+LAlcaide@users.noreply.github.com>
2026-08-08 08:16:04 +0800

    Update feeding_controller.v

--- FPGA-Dog-Feeder/rtl/feeding_controller.v
+++ FPGA-Dog-Feeder/rtl/feeding_controller.v
@@ -1,12 +1,12 @@
 `default_nettype none
 module feeding_controller(clk, fdismotor, rfidreg, eatenreg, loadcell, cloadcell, clearrfiddone, clearrfidreq, resetFoodLog, wloadcell, wcloadcell, wdismotor, loadcellreg, cloadcellreg, wloadcellreg, wcloadcellreg);
-	input clk, clearrfiddone, resetFoodLog;
-	input [1:0] rfidreg, loadcell, cloadcell, wloadcell, wcloadcell;
+	input wire clk, clearrfiddone, resetFoodLog;
+	input wire [1:0] rfidreg, loadcell, cloadcell, wloadcell, wcloadcell;
 	
-	output clearrfidreq;
-	output [3:0] fdismotor, wdismotor;
-	output [2:0] eatenreg;
-	output [1:0] loadcellreg, cloadcellreg, wloadcellreg, wcloadcellreg;
+	output wire clearrfidreq;
+	output wire [3:0] fdismotor, wdismotor;
+	output wire [2:0] eatenreg;
+	output  wire [1:0] loadcellreg, cloadcellreg, wloadcellreg, wcloadcellreg;
 	
 	reg [31:0] loadcelldel, wloadcelldel, wcloadcelldel, cloadcelldel;
 
