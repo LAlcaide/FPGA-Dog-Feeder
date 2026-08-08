f71eacb870e91be84797bbb2ca89b125a1cf292b
LAlcaide <108708146+LAlcaide@users.noreply.github.com>
2026-08-08 16:22:13 +0800

    Update rfid_controller.v

--- FPGA-Dog-Feeder/rtl/rfid_controller.v
+++ FPGA-Dog-Feeder/rtl/rfid_controller.v
@@ -1,11 +1,9 @@
 `default_nettype none
-module rfid_controller (clk,rfid, clearrfidreq, rfidreg, clearrfiddone);
-	input wire clk, clearrfidreq;
-	input wire [1:0] rfid;
-	
-	output reg [1:0] rfidreg;
-	output reg clearrfiddone;
-	
+module rfid_controller (
+input wire clk, clearrfidreq, 
+input wire [1:0] rfid, 
+output reg [1:0] rfidreg, 
+output reg clearrfiddone);
 	initial begin
 		//RFID
 		rfidreg<=0;
